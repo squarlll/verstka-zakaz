34da55e541faca2ce06aee5863a83f3417f0be9c
squarlll <lilsquarl@gmail.com>
2020-02-02 01:27:08 +0300

    dasd

--- js/5crm.js
+++ js/5crm.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
 		$('.menu__second-level').addClass('second-level-active');
 		$('.header__menu ul:first-child').addClass('header__menu-hidden')
 	})
-	$('.second-level__back i').on('click', function(){
+	$('.second-level__back').on('click', function(){
 		$('.menu__second-level').removeClass('second-level-active');
 		$('.header__menu ul:first-child').removeClass('header__menu-hidden')
 	})
