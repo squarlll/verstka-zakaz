637f94fdc513629f715bd5e3c4a2c16bf5579b8b
squarlll <lilsquarl@gmail.com>
2020-02-01 22:39:37 +0300

    счя

--- js/5crm.js
+++ js/5crm.js
@@ -2,6 +2,15 @@ $(document).ready(function(){
 
 	$('.header__menu-btn').on('click', function(){
 		$('.header__menu ul').toggleClass('menu-active');
+		$(this).toggleClass('hamburger-active');
+		$('.header').toggleClass('header-active');
+	});
+	$('.possibility').on('click', function(){
+		$('.menu__second-level').addClass('second-level-active');
+		$('.header__menu ul:first-child').addClass('header__menu-hidden')
 	})
-
+	$('.second-level__back i').on('click', function(){
+		$('.menu__second-level').removeClass('second-level-active');
+		$('.header__menu ul:first-child').removeClass('header__menu-hidden')
+	})
 })
