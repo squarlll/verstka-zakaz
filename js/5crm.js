035b27c425ec3dd3d54fc0ba5060b353d0edad2b
squarlll <lilsquarl@gmail.com>
2020-02-19 01:27:03 +0300

    авфы
    ва

--- js/5crm.js
+++ js/5crm.js
@@ -1,5 +1,4 @@
 $(document).ready(function(){
-
 	$('.header__menu-btn').on('click', function(){
 		$('.header__menu ul').toggleClass('menu-active');
 		$(this).toggleClass('hamburger-active');
@@ -13,4 +12,21 @@ $(document).ready(function(){
 		$('.menu__second-level').removeClass('second-level-active');
 		$('.header__menu ul:first-child').removeClass('header__menu-hidden')
 	})
-})
+});
+$(function() {
+    var elem = $.jInvertScroll(['.scroll'],        // an array containing the selector(s) for the elements you want to animate
+        {
+       // height: 6000,                   // optional: define the height the user can scroll, otherwise the overall length will be taken as scrollable height
+        onScroll: function(percent) {   //optional: callback function that will be called when the user scrolls down, useful for animating other things on the page
+            console.log(percent);
+        }
+    });
+    $(window).resize(function() {
+      if ($(window).width() <= 768) {
+        elem.destroy();
+      }
+      else {
+        elem.reinitialize();
+      }
+    });
+});
