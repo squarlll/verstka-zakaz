560a6b17b5a5f1f0486534bfecfd900bdc871253
squarlll <lilsquarl@gmail.com>
2020-03-03 00:24:08 +0300

    чывы

--- js/5crm.js
+++ js/5crm.js
@@ -7,52 +7,65 @@ $(document).ready(function(){
 	$('.second-level__back').on('click', function(){
 		$('.menu__second-level').removeClass('second-level-active');
 		$('.header__menu ul:first-child').removeClass('header__menu-hidden')
-	})
+	});
+    $('.header__search').on('click', function(){
+        $('.desktop__form').addClass('desktop__form-active');
+        $('.header__menu ul:nth-child(2), .header__client, .header__logo').addClass('header__menu-hidden');
+        $('body').addClass('html-fixed');
+        $('html').addClass('html-scroll');
+        $('.header__search-quick-wrapper').addClass('header__search-quick-wrapper-active');
+        $('.desktop__input').focus();
+
+        $('.form__button').on('click', function(){
+            $('.desktop__form').removeClass('desktop__form-active');
+            $('.header__menu ul:nth-child(2), .header__client, .header__logo').removeClass('header__menu-hidden');
+            $('body').removeClass('html-fixed');
+            $('.header__search-quick-wrapper').removeClass('header__search-quick-wrapper-active');
+        });
+    });
+
+    // MENU
+    $('.possibility').on('click', function windowSize(){
+        if ($(window).width() <= '768'){
+                $('.mobile-menu__possibility').addClass('second-level-active');
+                $('.header__menu ul:first-child').addClass('header__menu-hidden')
+        }
+        else {
+            $('.header-bottom__possibility').siblings().removeClass('menu-bottom_active')
+            $('.header-bottom__possibility').addClass('menu-bottom_active');
+            $('.header-bottom').toggleClass('db');
+            $('.header__contact').on('click', function(){
+                $('.header-bottom').removeClass('db')
+            });
+        }
+    });
+    $('.header__contact').on('click', function windowSize(){
+        if ($(window).width() <= '768'){
+                // $('.mobile-menu__contacts').toggleClass('second-level-active');
+                $('.mobile-menu__contacts').slideToggle();
+                $('.contact__menu').toggleClass('contact__menu-active');
+                $('.header__contact i').toggleClass('angle-active');
+        }
+        else {
+            $('.header-bottom__contact').siblings().removeClass('menu-bottom_active')
+            $('.header-bottom__contact').addClass('menu-bottom_active');
+            $('.header-bottom').toggleClass('db1');
+            $('.possibility').on('click', function(){
+                $('.header-bottom').removeClass('db1')
+            });
+        }
+    });
+
 });
+(function(){
 
+    $('.sticky-content').StickyDL({
+        paddingTop : 155,
+        heightRefElement: '.main-content-end-padding',
+        optionalBottomFix: 40
+    })
 
-
-// MENU
-
-$('.possibility').on('click', function windowSize(){
-    if ($(window).width() <= '768'){
-            $('.mobile-menu__possibility').addClass('second-level-active');
-            $('.header__menu ul:first-child').addClass('header__menu-hidden')
-    }
-    else {
-        $('.header-bottom__possibility').siblings().removeClass('menu-bottom_active')
-        $('.header-bottom__possibility').addClass('menu-bottom_active');
-        $('.header-bottom').css('display', 'block');
-    }
-});
-
-$('.header__contact').on('click', function windowSize(){
-    if ($(window).width() <= '768'){
-            // $('.mobile-menu__contacts').toggleClass('second-level-active');
-            $('.mobile-menu__contacts').slideToggle();
-            $('.contact__menu').toggleClass('contact__menu-active');
-            $('.header__contact i').toggleClass('angle-active');
-    }
-    else {
-        $('.header-bottom__contact').siblings().removeClass('menu-bottom_active')
-        $('.header-bottom__contact').addClass('menu-bottom_active');
-        $('.header-bottom').css('display', 'block');
-    }
-});
-
-
-// $('.header__help').on('click', function windowSize(){
-//     if ($(window).width() <= '910'){
-//             $('.menu__second-level').addClass('second-level-active');
-//             $('.header__menu ul:first-child').addClass('header__menu-hidden')
-//     }
-//     else {
-//             $('.header-bottom').html('<div>6</div>')
-//     }
-// });
-
-
-
+})
 $(function() {
     var elem = $.jInvertScroll(['.scroll'],        // an array containing the selector(s) for the elements you want to animate
         {
